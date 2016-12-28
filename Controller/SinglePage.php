fd7c463d32ba3c832b80686bdbe87e963d0042bd
Ali Shahghale <info.shahghale@gmail.com>
2016-12-29 00:34:32 +0330

    fix row_desc Static 'Yes/NO Pic'

--- Controller/SinglePage.php
+++ Controller/SinglePage.php
@@ -1,11 +1,12 @@
 <?php
 $url=(@$_POST['url']);
+$ret[]=$output[]='';
 if (!$url){
 	exit('لطفا لینک  محصول را وارد کنید');
 }else {
 	include('../Plugins/simple_html_dom.php');
-	$inner_html = file_get_html($url);
-	foreach ($inner_html->find('table.edman') as $o) {
+	$Single_product_html = file_get_html($url);
+	foreach ($Single_product_html->find('table.edman') as $o) {
 		$item['title'] = trim($o->find('span#product_info_name', 0)->innertext);
 		$item['price'] = trim($o->find('span.productPrice,span.productStatus', 0)->innertext);
 
@@ -14,7 +15,14 @@ if (!$url){
 		foreach ($o->find('table.pef_group_container_table') as $value) {
 			foreach ($value->find('tr.pef_inner_container_box_group_container_row_odd,tr.pef_inner_container_box_group_container_row_even') as $tr) {
 				$item['product']['feature']['row_title'][] = trim($tr->find('td.pef_inner_container_box_group_container_row_title', 0)->innertext);
-				$item['product']['feature']['row_desc'][] = trim($tr->find('td.pef_inner_container_box_group_container_row_value span', 0)->innertext);
+                 $ACT = trim($tr->find('td.pef_inner_container_box_group_container_row_value span', 0)->innertext);
+                if($ACT=='<img src="./images/icons/yes.png" />'){
+                    $item['product']['feature']['row_desc'][]='بله';
+                }elseif ($ACT=='<img src="./images/icons/no.png" />'){
+                    $item['product']['feature']['row_desc'][]='خیر';
+                }else{
+                  $item['product']['feature']['row_desc'][] = trim($tr->find('td.pef_inner_container_box_group_container_row_value span', 0)->innertext);
+                }
 			}
 		}
 		$cama = $item['tags'] = '';
@@ -23,8 +31,8 @@ if (!$url){
 			$cama = ',';
 		}
 	}
-	$ret[] = $item;
-	$output[] = $ret;
+	$ret = $item;
+	$output = $ret;
 	$ret = $item = '';
 	print_r($output);
 }
